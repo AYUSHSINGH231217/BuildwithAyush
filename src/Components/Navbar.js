81e55cb6bc4c43e8aa88edad11101cffebfc9dc1
Ayush Singh <ayushsingh231217@gmail.com>
2025-05-12 00:45:12 +0530

    Initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <nav className="Navbar">
+      <h1 title='Reload' onClick={() => window.location.reload(true)} className='Logo'>AS</h1>
+
+      <ul className='NavbarLinks'>
+        <li><Link to="/"> Home</Link></li>
+        <li><Link to="/About"> About</Link></li>
+        <li><Link to="/Project"> Project</Link></li>
+        <li><Link to="/Resume">Resume</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
